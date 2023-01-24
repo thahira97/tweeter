7b38fbc9d5e906002da42c405cb7be69b4cf933c
thahira97 <thahirasheerin3@gmail.com>
2023-01-24 11:26:31 -0500

    Add Function to render the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,42 +3,64 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function (){
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
       },
-    "created_at": 1461116232227
-  }
-  const createTweetElement = function (tweetData) {
-   const $tweet =`<article class="tweet">
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+  ///Function to render the tweets
+  const renderTweets = function (tweets) {
+    const $tweetsContainer = $("#tweets-container")
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetsContainer.prepend($tweet);
+    }
+  };
+  
+  ///Function to create one tweet
+  const createTweetElement = function (tweet) {
+    let $tweet= `<article class="tweet">
    <header class="tweet-header">
      <div class="username">
-       <span><img src = ${tweetData.user["avatars"]}</span>
-       <span>${tweetData.user["name"]}</span>
+       <span><img src = ${tweet.user["avatars"]}</span>
+       <span>${tweet.user["name"]}</span>
      </div>
-     <p class="user-id">${tweetData.user["handle"]}</p>
+     <p class="user-id">${tweet.user["handle"]}</p>
    </header>
    <div class="tweet-text">
-   ${tweetData.content["text"]}
+   ${tweet.content["text"]}
    </div>
    <footer class="tweet-footer">
-     <aside>${tweetData.created_at}</aside>
+     <aside>${tweet.created_at}</aside>
      <div class="tweet-actions">
        <i class="fa-solid fa-flag"></i>
        <i class="fa-solid fa-retweet"></i>
        <i class="fa-solid fa-heart"></i>
      </div>
    </footer>
- </article>`
-   ;return $tweet
+ </article>`;
+    return $tweet;
   };
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet)
-  $('#tweets-container').prepend($tweet);
-})
+
+  renderTweets(data);
+});
